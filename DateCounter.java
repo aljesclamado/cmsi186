9f2fdde2aba08d537cf93efe8b0558cf5a420039
aljesclamado <aesclam1@lion.lmu.edu>
2017-01-26 15:07:02 -0800

    stubbed everything minus main
    part of Part 0: Commit Practice

--- DateCounter.java
+++ DateCounter.java
@@ -2,14 +2,23 @@ import java.time.LocalDate;
 
 public class DateCounter {
 
-    /**
-     * Returns today's date as a three-element integer array in year-month-day order.
-     */
-     public static int[] today() {
-         LocalDate now = LocalDate.now();
-         return new int[] {
-             now.getYear(), now.getMonth().getValue(), now.getDayOfMonth()
-         };
-     }
+	public static boolean isLeapYear(int year) {
+		return true;
+	}
 
+	public static int daysInMonth(int year, int month) {
+		return 35;
+	}
+
+	public static boolean isValidDate(int year, int month, int day) {
+		return true;
+	}
+
+	public static int daysBetween(int year0, int month0, int day0, int year1, int month1, int day1) {
+		return 6;
+	}
+
+	public static int ageInDays(int birthyear, int birthmonth, int birthday) {
+		return 123;
+	}
 }
